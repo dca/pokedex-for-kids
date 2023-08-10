526e534db8c2976451f16305bec602150b1c75ea
Dca <dcahsu@gmail.com>
2023-08-10 21:00:57 +0800

    feat: Update icon, redirect home to pokedex-kids

--- pokedex-for-kids-web/src/app/pokedex-kids/[name]/page.tsx
+++
@@ -1,6 +0,0 @@
-import pokedex from '../../../../../crawlers/assets/base-pokedex.json'
-
-
-export default function Page({ params }: { params: { name: string } }) {
-    return <div>Hello: {params.name}</div>
-}
