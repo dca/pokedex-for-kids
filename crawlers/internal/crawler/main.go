fbaf80598d47756798fd14df978401602a4ef2f7
Dca <dcahsu@gmail.com>
2023-08-10 15:05:58 +0800

    feat: Add en names mp3

--- crawlers/internal/crawler/main.go
+++ crawlers/internal/crawler/main.go
@@ -11,10 +11,15 @@ func Start() {
 	}
 
 	for _, pokemon := range pokedexData {
-		err := SynthesizeText("en-US", pokemon.NameEn, "assets/mp3/" + pokemon.Index + "-" + pokemon.NameEn + ".mp3")
-		if err != nil {
-			panic(err)
-		}
+		// err := SynthesizeText("en-US", pokemon.NameEn, "assets/mp3/" + pokemon.Index + "-" + pokemon.NameEn + ".mp3")
+		// if err != nil {
+		// 	panic(err)
+		// }
+
+		// err := SynthesizeText("zh-TW", pokemon.NameTw, "assets/mp3/" + pokemon.Index + "-" + pokemon.NameTw + ".mp3")
+		// if err != nil {
+		// 	panic(err)
+		// }
 	}
 
 
